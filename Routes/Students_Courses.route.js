1588000d440f36e22b3de4954cf588a2324dd3af
Javed <javedahmednizamani@gmail.com>
2023-10-31 11:59:59 +0500

    Portfolio Project - I

--- Routes/Students_Courses.route.js
+++ Routes/Students_Courses.route.js
@@ -1,9 +1,10 @@
         // imports
 const express = require('express');             
-const route = express.Router();                 
+const route = express.Router(); 
+const {sanitizeForeignKeyValues} = require('../Middleware/sanitizer.middleware');                
 const {addForeignKeyValues} = require('../controller/Students_Courses.controller');
 const {auth} = require('../Middleware/login-auth');                        
     // route to add foreign keys
-route.post('/addForeignKey',auth,addForeignKeyValues);           
+route.post('/addForeignKey',auth,sanitizeForeignKeyValues,addForeignKeyValues);           
 
 module.exports = route                  // export route  
